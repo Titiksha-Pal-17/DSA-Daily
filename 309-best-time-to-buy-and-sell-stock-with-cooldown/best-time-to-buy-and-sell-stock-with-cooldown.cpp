aa8d809e22942367cd111b1b34714f7a92a6d68f
Titiksha Pal <titikshapal765@gmail.com>
2026-08-11 10:55:18 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (36.75%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+   long f(int index , int buy , vector<int> &prices , vector<vector<int>>& dp ){
+        int n =prices.size();
+        if(index >= n) return 0;
+        if( dp[index][buy]!= -1) return dp[index][buy];
+        
+        long profit=0;
+        
+        if(buy){
+            
+            profit = max( -prices[index] + f(index+1 , 0 , prices , dp)  , 
+            0 + f(index+1 , 1,prices , dp)) ;
+            
+        }
+        
+        else{      //selling or not selling  but not buying wirh cooldown 
+                    //index+2 for skipping one 
+            profit= max( prices[index] + f(index+2 , 1 , prices , dp)   , 
+            0+ f(index+1 , 0 ,prices , dp)); // only skipping in the second half so no change
+        }
+        
+        return dp[index][buy]= profit ;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp( n , vector<int>(2 , -1) );
+
+        return f( 0 , 1 , prices , dp);
+  }
+};
